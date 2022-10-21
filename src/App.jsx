a30e6a9162fe89d419728890058ca372bc34091c
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-10-21 00:02:11 -0400

    feat: scaffold application, added form and post components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App
